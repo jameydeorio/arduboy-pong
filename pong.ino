91f0e5b86e6d6be023b0d3f6df4c256e28ea4897
Jamey J. DeOrio <jdeorio@safaribooksonline.com>
2019-10-30 23:05:23 -0700

    Use scope blocks

--- pong.ino
+++ pong.ino
@@ -3,6 +3,7 @@
 Arduboy2 arduboy;
 
 const int FPS = 60;
+const int WINNING_SCORE = 1;
 
 int gameState = 0;
 
@@ -52,158 +53,170 @@ void loop() {
 
   switch(gameState) {
     case 0: // title screen
-      arduboy.setCursor(0, 0);
-      arduboy.println("wow it's pong");
-      arduboy.println("---===---");
-      arduboy.println();
-      arduboy.println("A - power shot");
-      arduboy.println("B - fast move");
-      arduboy.println();
-      arduboy.println("Press A to start");
-      if (arduboy.justPressed(A_BUTTON)) {
-        gameState += 1;
+      {
+        arduboy.setCursor(0, 0);
+        arduboy.println("wow it's pong");
+        arduboy.println("---===---");
+        arduboy.println();
+        arduboy.println("A - power shot");
+        arduboy.println("B - fast move");
+        arduboy.println();
+        arduboy.println("Press A to start");
+        if (arduboy.justPressed(A_BUTTON)) {
+          gameState += 1;
+        }
       }
       break;
 
     case 1: // gameplay
-      //--- player input
-      if (arduboy.pressed(B_BUTTON)) {
-        // fast move
-        player.speed = 2;
-      } else {
-        player.speed = 1;
-      }
-      if (arduboy.pressed(UP_BUTTON) && player.y > 1) {
-        player.y -= 1 * player.speed;
-      }
-      if (arduboy.pressed(DOWN_BUTTON) && player.y + player.height < arduboy.height() - 1) {
-        player.y += 1 * player.speed;
-      }
-
-      //--- computer movement
-      if (random(30) == 1) {
-        // random fast move
-        computer.speed = 2;
-      } else {
-        computer.speed = 1;
-      }
-      if (ball.x > arduboy.width() / 5 * 4 || random(20) == 1) {
-        // only move when ball is close (or random chance)
-        if (ball.y < computer.y && computer.y > 1) {
-          computer.y -= computer.speed;
-        }
-        if (ball.y > computer.y && computer.y + computer.height < arduboy.height() - 1) {
-          computer.y += computer.speed;
-        }
-      }
-
-      //--- player collision
-      if (
-        ball.x < player.x + player.width
-        && ball.y + ball.size > player.y
-        && ball.y < player.y + player.height
-        && ball.rightDir < 0
-      ) {
-        // reverse direction
-        ball.rightDir *= -1;
-        if (arduboy.pressed(A_BUTTON)) {
-          // power shot
-          ball.xspeed = 2;
+      {
+        //--- player input
+        if (arduboy.pressed(B_BUTTON)) {
+          // fast move
+          player.speed = 2;
         } else {
-          // lasts until ball is returned
+          player.speed = 1;
+        }
+        if (arduboy.pressed(UP_BUTTON) && player.y > 1) {
+          player.y -= 1 * player.speed;
+        }
+        if (arduboy.pressed(DOWN_BUTTON) && player.y + player.height < arduboy.height() - 1) {
+          player.y += 1 * player.speed;
+        }
+
+        //--- computer movement
+        if (random(30) == 1) {
+          // random fast move
+          computer.speed = 2;
+        } else {
+          computer.speed = 1;
+        }
+        if (ball.x > arduboy.width() / 5 * 4 || random(20) == 1) {
+          // only move when ball is close (or random chance)
+          if (ball.y < computer.y && computer.y > 1) {
+            computer.y -= computer.speed;
+          }
+          if (ball.y > computer.y && computer.y + computer.height < arduboy.height() - 1) {
+            computer.y += computer.speed;
+          }
+        }
+
+        //--- player collision
+        if (
+          ball.x < player.x + player.width
+          && ball.y + ball.size > player.y
+          && ball.y < player.y + player.height
+          && ball.rightDir < 0
+        ) {
+          // reverse direction
+          ball.rightDir *= -1;
+          if (arduboy.pressed(A_BUTTON)) {
+            // power shot
+            ball.xspeed = 2;
+          } else {
+            // lasts until ball is returned
+            ball.xspeed = 1;
+          }
+          if (isEdgeHit(player)) {
+            ball.yspeed = 2;
+          } else {
+            ball.yspeed = 1;
+          }
+        }
+
+        //--- computer collision
+        if (
+          ball.x + ball.size > computer.x
+          && ball.y + ball.size > computer.y
+          && ball.y < computer.y + computer.height
+          && ball.rightDir > 0
+        ) {
+          ball.rightDir = -1;
+          if (random(10) == 1) {
+            // random power shot
+            ball.xspeed = 2;
+          }
+          if (isEdgeHit(computer)) {
+            ball.yspeed = 2;
+          } else {
+            ball.yspeed = 1;
+          }
+        }
+
+        //--- ball movement
+        ball.x += ball.rightDir * ball.xspeed;
+        ball.y += ball.downDir * ball.yspeed;
+
+        //--- bounds collision
+        if (ball.y <= 0) {
+          ball.downDir = 1;
+        }
+        if (ball.y + ball.size >= 63) {
+          ball.downDir = -1;
+        }
+
+        //--- points
+        bool outOfBoundsLeft = ball.x <= -4;
+        bool outOfBoundsRight = ball.x >= arduboy.width() + 4;
+        bool outOfBounds = outOfBoundsLeft || outOfBoundsRight;
+        if (outOfBounds) {
           ball.xspeed = 1;
-        }
-        if (isEdgeHit(player)) {
-          ball.yspeed = 2;
-        } else {
           ball.yspeed = 1;
         }
-      }
-
-      //--- computer collision
-      if (
-        ball.x + ball.size > computer.x
-        && ball.y + ball.size > computer.y
-        && ball.y < computer.y + computer.height
-        && ball.rightDir > 0
-      ) {
-        ball.rightDir = -1;
-        if (random(10) == 1) {
-          // random power shot
-          ball.xspeed = 2;
+        if (outOfBoundsLeft) {
+          computer.score += 1;
+          ball.x = arduboy.width() / 2;
+          ball.y = random(10, arduboy.height()-10);
         }
-        if (isEdgeHit(computer)) {
-          ball.yspeed = 2;
-        } else {
-          ball.yspeed = 1;
+        if (outOfBoundsRight) {
+          player.score += 1;
+          ball.x = arduboy.width() / 2;
+          ball.y = random(10, arduboy.height()-10);
         }
+
+        if (player.score == WINNING_SCORE) {
+          gameState = 2;
+        }
+        if (computer.score == WINNING_SCORE) {
+          gameState = 3;
+        }
+
+        //--- draw the things
+        // bounds
+        arduboy.drawLine(0, 0, arduboy.width(), 0, WHITE);
+        arduboy.drawLine(0, arduboy.height()-1, arduboy.width(), arduboy.height()-1, WHITE);
+        // players
+        arduboy.fillRect(player.x, player.y, player.width, player.height, WHITE);
+        arduboy.fillRect(computer.x, computer.y, computer.width, computer.height, WHITE);
+        // ball
+        arduboy.fillRect(ball.x, ball.y, ball.size, ball.size, WHITE);
+
+        arduboy.setCursor(arduboy.width() / 2 - 14, 2);
+        arduboy.print(player.score);
+        arduboy.setCursor(arduboy.width() / 2 - 2, 2);
+        arduboy.print("|");
+        arduboy.setCursor(arduboy.width() / 2 + 10, 2);
+        arduboy.print(computer.score);
       }
-
-      //--- ball movement
-      ball.x += ball.rightDir * ball.xspeed;
-      ball.y += ball.downDir * ball.yspeed;
-
-      //--- bounds collision
-      if (ball.y <= 0) {
-        ball.downDir = 1;
-      }
-      if (ball.y + ball.size >= 63) {
-        ball.downDir = -1;
-      }
-
-      //--- points
-      bool outOfBoundsLeft = ball.x <= 0;
-      bool outOfBoundsRight = ball.x >= arduboy.width();
-      bool outOfBounds = outOfBoundsLeft || outOfBoundsRight;
-      if (outOfBounds) {
-        ball.xspeed = 1;
-        ball.yspeed = 1;
-      }
-      if (outOfBoundsLeft) {
-        computer.score += 1;
-        ball.x = arduboy.width() / 2;
-        ball.y = random(10, arduboy.height()-10);
-      }
-      if (outOfBoundsRight) {
-        player.score += 1;
-        ball.x = arduboy.width() / 2;
-        ball.y = random(10, arduboy.height()-10);
-      }
-
-      //--- draw the things
-      // bounds
-      arduboy.drawLine(0, 0, arduboy.width(), 0, WHITE);
-      arduboy.drawLine(0, arduboy.height()-1, arduboy.width(), arduboy.height()-1, WHITE);
-      // players
-      arduboy.fillRect(player.x, player.y, player.width, player.height, WHITE);
-      arduboy.fillRect(computer.x, computer.y, computer.width, computer.height, WHITE);
-      // ball
-      arduboy.fillRect(ball.x, ball.y, ball.size, ball.size, WHITE);
-
-      arduboy.setCursor(arduboy.width() / 2 - 14, 2);
-      arduboy.print(player.score);
-      arduboy.setCursor(arduboy.width() / 2 - 2, 2);
-      arduboy.print("|");
-      arduboy.setCursor(arduboy.width() / 2 + 10, 2);
-      arduboy.print(computer.score);
-
-      // debug
-
       break;
 
     case 2: // win screen
-      arduboy.setCursor(0, 0);
-      arduboy.print("win screen");
-      if (arduboy.justPressed(B_BUTTON)) {
-        gameState += 1;
+      {
+        arduboy.setCursor(0, 0);
+        arduboy.print("you win");
+        if (arduboy.justPressed(A_BUTTON)) {
+          gameState += 1;
+        }
       }
       break;
 
     case 3: // game over screen
-      arduboy.setCursor(0, 0);
-      arduboy.print("game over");
-      if (arduboy.justPressed(B_BUTTON)) {
-        gameState = 0;
+      {
+        arduboy.setCursor(0, 0);
+        arduboy.print("game over");
+        if (arduboy.justPressed(A_BUTTON)) {
+          gameState = 0;
+        }
       }
       break;
   }
